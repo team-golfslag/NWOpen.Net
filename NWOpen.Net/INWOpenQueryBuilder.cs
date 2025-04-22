e31b4f84157f586c4c054c99d131931e41b93587
Geert Haans <geert.haans@gmail.com>
2025-04-22 10:42:17 +0200

    feat: add summaries

--- NWOpen.Net/INWOpenQueryBuilder.cs
+++ NWOpen.Net/INWOpenQueryBuilder.cs
@@ -12,24 +12,73 @@ namespace NWOpen.Net;
 /// </summary>
 public interface INWOpenQueryBuilder
 {
+    /// <summary>
+    /// Filter by project organization.
+    /// </summary>
+    /// <param name="organization">The organization name to filter by.</param>
+    /// <remarks> Can only be called once. </remarks>
     INWOpenQueryBuilder WithOrganization(string organization);
+    
+    /// <summary>
+    /// Filter by project title.
+    /// </summary>
+    /// <param name="title">The title to filter by.</param>
+    /// <param name="exact">Whether the title should be an exact match.</param>
     INWOpenQueryBuilder WithTitle(string title, bool exact = false);
+    
+    /// <summary>
+    /// Filter by project role.
+    /// </summary>
+    /// <param name="role">The role to filter by.</param>
     INWOpenQueryBuilder WithRole(string role);
+    
+    /// <summary>
+    /// Filter by project member last name.
+    /// </summary>
+    /// <param name="lastName">The last name to filter by.</param>
     INWOpenQueryBuilder WithMemberLastName(string lastName);
+    
+    /// <summary>
+    /// Set the project start date beginning of the range.
+    /// </summary>
+    /// <param name="from">The start date beginning of the range.</param>
     INWOpenQueryBuilder WithStartDateFrom(DateTime from);
+    
+    /// <summary>
+    /// Set the project start date end of the range.
+    /// </summary>
+    /// <param name="until">The start date end of the range.</param>
     INWOpenQueryBuilder WithStartDateUntil(DateTime until);
+    
+    /// <summary>
+    /// Set the project end date beginning of the range.
+    /// </summary>
+    /// <param name="from">The end date beginning of the range.</param>
     INWOpenQueryBuilder WithEndDateFrom(DateTime from);
+    
+    /// <summary>
+    /// Set the project end date end of the range.
+    /// </summary>
+    /// <param name="until">The end date end of the range.</param>
     INWOpenQueryBuilder WithEndDateUntil(DateTime until);
 
     /// <summary>
-    /// How many results to return.
+    /// Set the number of results to return.
     /// </summary>
+    /// <param name="numberOfResults">The number of results to return.</param>
+    /// <remarks> Must be greater than 0. Can only be set once. </remarks>
     INWOpenQueryBuilder WithNumberOfResults(int numberOfResults);
     
+    /// <summary>
+    /// Build the query.
+    /// </summary>
+    /// <returns> The queries. </returns>
+    /// <remarks> returns more than 1 query when the number of results is greater than the page size. </remarks>
     List<string> BuildQueries();
 
     /// <summary>
     /// Execute the query.
     /// </summary>
+    /// <returns> The result of the query. </returns>
     Task<NWOpenResult?> ExecuteAsync();
 }
